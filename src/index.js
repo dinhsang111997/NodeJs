58ba5b6db0276a9bb3e89c0f9406e2aec00bfcc6
dinhsang111997 <dinhsang111997@gmail.com>
2021-12-02 10:32:48 +0700

    engine template trong ExpressJs

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+//phai them {engine}
+const { engine } = require("express-handlebars");
+var morgan = require("morgan");
+
+// const { extname } = require("path/posix");
+
+const app = express();
+
+app.use(morgan("combined"));
+//đổi file .handlebars thành .hbs
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(3000);
